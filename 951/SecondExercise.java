6d93d3a5bc5ea277d295aa05fa6ea241c681b058
JavaRavi <34348125+JavaRavi@users.noreply.github.com>
2017-12-24 17:34:33 -0500

    Add files via upload

---
+++ 951/SecondExercise.java
@@ -0,0 +1,18 @@
+package com.htc.exercies;
+
+public class SecondExercise {
+
+	public SecondExercise() {
+		// TODO Auto-generated constructor stub
+	}
+   public static void main(String args[]){
+	    Project pr = new Project();
+	    pr.setProjectId("PR1");
+	    pr.setProjectName("Apollo -1");
+	    pr.setProjectHead("Tom Smith");
+	    pr.setNoOfResources(20);
+	    
+	   System.out.println("First Object Created " +pr);
+	    
+   }
+}
