a974f282edfa13268caa3faf7e6c9e07bb331705
prakash04134 <prakash04134@gmail.com>
2017-12-26 21:53:27 -0600

    Added Batch Class
    Signed-off-by: prakash04134 <prakash04134@gmail.com>

---
+++ 1984/src/Batch.java
@@ -0,0 +1,15 @@
+import java.sql.Timestamp;
+
+
+public class Batch {
+
+	public int batchCode;
+	public Timestamp startDate;
+	public Timestamp endDate;
+	public Trainee[] traineeField;
+	
+
+	
+	
+	
+}
--- 1984/src/Trainee.java
+++ 1984/src/Trainee.java
@@ -1,9 +1,13 @@
 
 public class Trainee {
 
+	public int traineeID;
+	public String traineeName;
+	public double contactnumber;
+	public String email;
+	public int age;
 	public static void main(String[] args) {
-		// TODO Auto-generated thod stub
-
+		
 	}
 
 }
