a9bda5f889ea0d10a7b73298604a2d98a7829a6b
prakash04134 <prakash04134@gmail.com>
2017-12-27 21:24:06 -0600

    batch class file update

--- 1984/src/Batch.java
+++ 1984/src/Batch.java
@@ -2,11 +2,46 @@ import java.sql.Timestamp;
 
 
 public class Batch {
+	
+	private int batchCode;
+	private Timestamp startDate;
+	private Timestamp endDate;
+	private Trainee[] traineeField;
 
-	public int batchCode;
-	public Timestamp startDate;
-	public Timestamp endDate;
-	public Trainee[] traineeField;
+	public int getBatchCode() {
+		return batchCode;
+	}
+	public void setBatchCode(int batchCode) {
+		this.batchCode = batchCode;
+	}
+	public Timestamp getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Timestamp startDate) {
+		this.startDate = startDate;
+	}
+	public Timestamp getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Timestamp endDate) {
+		this.endDate = endDate;
+	}
+	public Trainee[] getTraineeField() {
+		return traineeField;
+	}
+	public Trainee setTraineeField(Trainee[] traineeField) {
+		this.traineeField = traineeField;
+		
+		Trainee GetTrainee(int id)
+		{
+			Trainee objtraine=new Trainee(1,"bob",(int) 3098259758L,"bob@htcinc.com",50);
+			return objtraine;
+
+			
+		}
+		
+	}
+
 	
 
 	
--- 1984/src/Trainee.java
+++ 1984/src/Trainee.java
@@ -3,9 +3,20 @@ public class Trainee {
 
 	public int traineeID;
 	public String traineeName;
-	public double contactnumber;
+	public int contactnumber;
 	public String email;
 	public int age;
+	public Trainee (int id,String name,int contactnumber,String email,int age)
+	{
+	this.traineeID=id;
+	this.traineeName=name;
+     this.contactnumber=contactnumber;
+     this.email=email;
+     this.age=age;
+	}
+	public Trainee() {
+		// TODO Auto-generated constructor stub
+	}
 	public static void main(String[] args) {
 		
 	}
